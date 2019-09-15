73feb7aedbe4ebd4ebc2a8f03119d9737ae6f750
Max Rabe <rabe.maximilian@gmail.com>
2019-09-15 12:14:31 +0200

    Initial push of project files

---
+++ R/equations.R
@@ -0,0 +1,242 @@
+
+setClass("expr_num")
+setClass("expr_frac", slots = c(num = "integer", den = "integer"), prototype = list(num = 0L, den = 1L), contains = "expr_num")
+setMethod("show", "expr_frac", function(object) if(object@den!=1L) cat(sprintf("%d/%d", object@num, object@den)) else cat(object@num))
+setClass("expr_real", slots = c(num = "numeric"), prototype = list(num = 0), contains = "expr_num")
+setMethod("show", "expr_real", function(object) cat(object@num))
+setClass("expr_coef", slots = c(num = "expr_num", var = "character"))
+setMethod("show", "expr_coef", function(object) {
+  if(as.numeric.expr_num(object@num) != 1 || length(object@var) == 0) {
+    if(as.numeric.expr_num(object@num) == -1) {
+      cat("-")
+    } else {
+      show(object@num)
+      if(length(object@var) > 0) {
+        cat("*")
+      }
+    }
+  }
+  cat(paste(object@var, collapse = "*"))
+})
+setClass("expr_sum", contains = "list")
+setMethod("show", "expr_sum", function(object) {
+  if(length(object) > 0) {
+    for(i in seq_along(object)) {
+      if(i>1) {
+        if(as.numeric.expr_num(object[[i]]@num) >= 0) {
+          cat(" + ")
+          show(object[[i]])
+        } else {
+          cat(" - ")
+          show(multiply_expr(object[[i]], new("expr_coef", num = new("expr_frac", num = -1L))))
+        }
+      } else {
+        show(object[[i]])
+      }
+    }
+  } else {
+    cat("0")
+  }
+})
+
+
+as.numeric.expr_num <- function(x) {
+  if(is(x, "expr_frac")) x@num/x@den
+  else if(is(x, "expr_real")) x@num
+  else as.numeric(x)
+}
+
+as.fractions.expr_num <- function(x) {
+  if(is(x, "expr_frac")) as.fractions(x@num)/as.fractions(x@den)
+  else as.fractions(x@num)
+}
+
+setMethod("as.numeric", signature(x="expr_num"), as.numeric.expr_num)
+#setMethod("as.fractions", signature(x="expr_num"), as.fractions.expr_num)
+
+`*.expr_num` <- function(a, b) {
+  if(is(a, "expr_frac") && is(b, "expr_frac")) {
+    new("expr_frac", num = a@num*b@num, den = a@den*b@den)
+  } else {
+    new("expr_real", num = as.numeric.expr_num(a)*as.numeric.expr_num(b))
+  }
+}
+#setMethod("*", signature("expr_num", "expr_num"), function(e1, e2) `*.expr_num`(e1, e2))
+
+`/.expr_num` <- function(a, b) {
+  b <- if(is(b, "expr_frac")) new("expr_frac", num = b@den, den = b@num) else new("expr_real", num = 1/as.numeric.expr_num(b))
+  `*.expr_num`(a, b)
+}
+#setMethod("/", signature("expr_num", "expr_num"), function(e1, e2) `/.expr_num`(e1, e2))
+
+`+.expr_num` <- function(a, b) {
+  if(is(a, "expr_frac") && is(b, "expr_frac")) {
+    if(a@den!=b@den) {
+      lcm <- as.integer(pracma::Lcm(a@den, b@den))
+      new("expr_frac", num = a@num * lcm %/% a@den +  b@num * lcm %/% b@den, den = lcm)
+    } else {
+      new("expr_frac", num = a@num+b@num, den = a@den)
+    }
+  } else {
+    new("expr_real", num = as.numeric.expr_num(a)+as.numeric.expr_num(b))
+  }
+}
+#setMethod("+", signature("expr_num", "expr_num"), function(e1, e2) `+.expr_num`(e1, e2))
+
+`-.expr_num` <- function(a, b) {
+  b <- if(is(b, "expr_frac")) new("expr_frac", num = -b@num, den = b@den) else new("expr_real", num = -as.numeric.expr_num(b@num))
+  `+.expr_num`(a, b)
+}
+#setMethod("-", signature("expr_num", "expr_num"), function(e1, e2) `-.expr_num`(e1, e2))
+#setMethod("-", signature("expr_num"), function(e1) new("expr_frac", num=0L, den=1L) - e1)
+
+`==.expr_num` <- function(a, b) as.numeric.expr_num(a) == as.numeric.expr_num(b)
+#setMethod("==", signature("expr_num", "expr_num"), function(e1, e2) `==.expr_num`(e1, e2))
+`<.expr_num` <- function(a, b) as.numeric.expr_num(a) < as.numeric.expr_num(b)
+#setMethod("<", signature("expr_num", "expr_num"), function(e1, e2) `<.expr_num`(e1, e2))
+#setMethod("<=", signature("expr_num", "expr_num"), function(e1, e2) `<.expr_num`(e1, e2) || `==.expr_num`(e1, e2))
+`>.expr_num` <- function(a, b) as.numeric.expr_num(a) > as.numeric.expr_num(b)
+#setMethod(">", signature("expr_num", "expr_num"), function(e1, e2) `>.expr_num`(e1, e2))
+#setMethod(">=", signature("expr_num", "expr_num"), function(e1, e2) `>.expr_num`(e1, e2) || `==.expr_num`(e1, e2))
+
+multiply_expr <- function(rh, mult) {
+  if(is(mult, "expr_coef")) {
+    if(is(rh, "expr_coef")) {
+      rh@num <- `*.expr_num`(mult@num, rh@num)
+      rh@var <- sort(c(mult@var, rh@var))
+      return(rh)
+    } else if(is(rh, "expr_sum")) {
+      for(i in seq_along(rh)) {
+        rh[[i]]@num <- `*.expr_num`(mult@num, rh[[i]]@num)
+        rh[[i]]@var <- sort(c(mult@var, rh[[i]]@var))
+      }
+      return(rh)
+    }
+  } else if(is(mult, "expr_sum")) {
+    if(is(rh, "expr_coef")) {
+      for(i in seq_along(mult)) {
+        mult[[i]]@num <- `*.expr_num`(mult[[i]]@num, rh@num)
+        mult[[i]]@var <- sort(c(mult[[i]]@var, rh@var))
+      }
+      return(mult)
+    } else if(is(rh, "expr_sum")) {
+      ret <- new("expr_sum")
+      for(i in seq_along(rh)) {
+        for(j in seq_along(mult)) {
+          ret[[length(ret)+1]] <- multiply_expr(rh[[i]], mult[[j]])
+        }
+      }
+      return(simplify_expr_sum(ret))
+    }
+  }
+  stop(sprintf("Unknown case for `mult` (classes %s and %s)!", class(rh), class(mult)))
+}
+
+simplify_expr_sum <- function(expr) {
+  ret <- list()
+  for(el in expr) {
+    if(el@num == 0) {
+      next
+    }
+    found <- FALSE
+    for(i in seq_along(ret)) {
+      if(setequal_exact(ret[[i]]@var, el@var)) {
+        f <- `+.expr_num`(ret[[i]]@num, el@num)
+        ret[[i]]@num <- f
+        found <- TRUE
+        break
+      }
+    }
+    if(!found) {
+      ret[[length(ret)+1]] <- el
+    }
+  }
+  return(as(ret[vapply(ret, function(x) as.numeric.expr_num(x@num) != 0, logical(1))], "expr_sum"))
+}
+
+setequal_exact <- function(x, y) {
+  for(el in x) {
+    i <- match(el, y)
+    if(is.na(i)) {
+      return(FALSE)
+    } else {
+      y <- y[-i]
+    }
+  }
+  return(length(y) == 0L)
+}
+
+simplify_expr <- function(expr) {
+  if(is.expression(expr)) {
+    ret <- simplify_expr(expr[[1]])
+    if(is(ret, "expr_num")) {
+      ret <- new("expr_coef", num = ret)
+    }
+    if(is(ret, "expr_coef")) {
+      ret <- as(list(ret), "expr_sum")
+    }
+    return(as(ret[vapply(ret, function(x) as.numeric.expr_num(x@num) != 0, logical(1))], "expr_sum"))
+  } else if(is.numeric(expr)) {
+    if(expr %% 1 == 0) {
+      num <- new("expr_frac", num = as.integer(expr), den = 1L)
+    } else {
+      num <- new("expr_real", num = expr)
+    }
+    return(new("expr_coef", num = num))
+  } else if(is.symbol(expr)) {
+    return(new("expr_coef", num = new("expr_frac", num = 1L, den = 1L), var = as.character(expr)))
+  } else if(is.call(expr)) {
+    if(expr[[1]] == "-") {
+      if(length(expr) == 3) {
+        return(simplify_expr(call("+", expr[[2]], call("*", expr[[3]], -1L))))
+      } else {
+        return(simplify_expr(call("*", expr[[2]], -1L)))
+      }
+    } else if(expr[[1]] == "/") {
+      if(!is.numeric(expr[[3]])) {
+        stop("Denominators must be numeric!")
+      }
+      if(is.numeric(expr[[2]])) {
+        if(expr[[2]] %% 1 == 0 && expr[[3]] %% 1 == 0) {
+          num <- new("expr_frac", num = as.integer(expr[[2]]), den = as.integer(expr[[3]]))
+        } else {
+          num <- new("expr_real", num = expr[[2]]/expr[[3]])
+        }
+        return(new("expr_coef", num = num))
+      } else {
+        lh <- simplify_expr(expr[[2]])
+        if(expr[[3]] %% 1 == 0) {
+          num <- new("expr_frac", num = 1L, den = as.integer(expr[[3]]))
+        } else {
+          num <- new("expr_real", num = 1/expr[[3]])
+        }
+        return(multiply_expr(lh, new("expr_coef", num = num)))
+      }
+    } else if(expr[[1]] == "*") {
+      lh <- simplify_expr(expr[[2]])
+      rh <- simplify_expr(expr[[3]])
+      return(multiply_expr(lh, rh))
+    } else if(expr[[1]] == "(") {
+      return(simplify_expr(expr[[2]]))
+    } else if(expr[[1]] == "+") {
+      lh <- simplify_expr(expr[[2]])
+      rh <- simplify_expr(expr[[3]])
+      if(is(lh, "expr_coef") && is(rh, "expr_coef")) {
+        ret <- new("expr_sum")
+        ret[[1]] <- lh
+        ret[[2]] <- rh
+      } else if(is(lh, "expr_coef") && is(rh, "expr_sum")) {
+        ret <- as(c(list(lh), rh), "expr_sum")
+      } else if(is(lh, "expr_sum") && is(rh, "expr_coef")) {
+        ret <- as(c(lh, list(rh)), "expr_sum")
+      } else {
+        stop(sprintf("Unknown summands %s and %s!", class(lh), class(rh)))
+      }
+      return(simplify_expr_sum(ret))
+    } else {
+      stop(sprintf("Unknown function `%s`!", as.character(expr[[1]])))
+    }
+  } else {
+    stop(sprintf("Unknown expression `%s`!", as.character(expr)))
+  }
+}
---
+++ R/hypr.R
@@ -0,0 +1,231 @@
+#' @include equations.R
+NULL
+
+
+#' An object containing null hypotheses, a hypothesis matrix, and a contrast matrix
+#'
+#' @slot eqs A list of null hypotheses
+#' @slot hmat A hypothesis matrix
+#' @slot cmat A contrast matrix
+#'
+setClass("hypr", slots=c(eqs = "list", hmat = "matrix", cmat = "matrix"))
+
+#' @describeIn hypr Print summary of hypr object
+#'
+#' @export
+setMethod(show, "hypr", function(object) {
+  if(length(object@eqs) == 0) {
+    cat("This object does not contain hypotheses.")
+  } else {
+    if(length(object@eqs) == 1) {
+      cat("1 null hypotheses:")
+    } else {
+      cat(sprintf("%d null hypotheses:", length(object@eqs)))
+    }
+    cat("\n")
+    for(i in seq_along(object@eqs)) {
+      cat(sprintf("% 3d: 0 = ", i))
+      show(object@eqs[[i]])
+      cat("\n")
+    }
+    cat("\nHypothesis matrix:\n")
+    show(as.fractions(object@hmat))
+    cat("\nContrast matrix:\n")
+    show(as.fractions(object@cmat))
+  }
+})
+
+parse_hypothesis <- function(expr, valid_terms = NULL) {
+  if(!is(expr, "formula")) {
+    stop("`expr` must be a formula")
+  }
+  ret <- simplify_expr_sum(simplify_expr(call("-",expr[[2]],expr[[3]])))
+  for(el in ret) {
+    if(length(el@var) == 0) {
+      stop("Equation should not have terms without variables!")
+    }
+    if(!is.null(valid_terms) && !all(el@var %in% valid_terms)) {
+      stop("Equation contains invalid variables!")
+    }
+  }
+  ret
+}
+
+#' Shorthand versions for simple hypothesis translation
+#'
+#' @param eqs A list() of equations
+#' @param terms (optional) A character vector of variables to be expected (if not provided, automatically generated from all terms occurring in the equations list)
+#' @param cmat A contrast matrix
+#' @param hmat A hypothesis matrix
+#' @return A list of equations (hmat2eqs and cmat2eqs), a contrast matrix (hmat2cmat, eqs2cmat), or a hypothesis matrix (cmat2hmat, eqs2hmat)
+#'
+#' @export
+eqs2hmat <- function(eqs, terms = NULL) {
+  if(is.null(terms)) {
+    terms <- unique(unlist(lapply(eqs, function(h) unlist(lapply(h, function(x) x@var)))))
+  }
+  as.matrix(vapply(seq_along(eqs), function(i) {
+    vapply(terms, function(j) {
+      for(el in eqs[[i]]) {
+        if(setequal_exact(el@var, j)) {
+          return(as.fractions.expr_num(el@num))
+        }
+      }
+      return(0)
+    }, double(1))
+  }, double(length(terms))))
+}
+
+#' @describeIn eqs2hmat Convert null hypothesis equations to contrast matrix
+#' @export
+eqs2cmat <- function(eqs) hmat2cmat(eqs2hmat(eqs))
+
+#' @describeIn eqs2hmat Convert hypothesis matrix to contrast matrix
+#' @export
+hmat2cmat <- function(hmat) {
+  cmat <- round(MASS::ginv(t(hmat)), 8)
+  rownames(cmat) <- rownames(hmat)
+  cmat
+}
+
+#' @describeIn eqs2hmat Convert contrast matrix to hypothesis matrix
+#' @export
+cmat2hmat <- function(cmat) {
+  hmat <- round(t(MASS::ginv(cmat)), 8)
+  rownames(hmat) <- rownames(cmat)
+  hmat
+}
+
+#' @describeIn eqs2hmat Convert hypothesis matrix to null hypothesis equations
+#' @export
+hmat2eqs <- function(hmat, as_fractions = TRUE) {
+  lapply(seq_len(ncol(hmat)), function(j) {
+    simplify_expr_sum(
+      as(lapply(seq_len(nrow(hmat)), function(i) {
+        if(as_fractions) {
+          frac <- strsplit(attr(MASS::as.fractions(hmat[i,j]), "fracs"), "/", TRUE)[[1]]
+          num <- new("expr_frac", num = as.integer(frac[1]), den = if(length(frac)>1) as.integer(frac[2]) else 1L)
+        } else {
+          num <- new("expr_real", num = hmat[i,j])
+        }
+        new("expr_coef", num = num, var = rownames(hmat)[i])
+      }), "expr_sum")
+    )
+  })
+}
+
+#' @describeIn eqs2hmat Convert contrast matrix to null hypothesis equations
+#' @export
+cmat2eqs <- function(cmat) hmat2eqs(cmat2hmat(cmat))
+
+#' Create a hypr object
+#'
+#' @param ... A list of null hypothesis equations
+#'
+#' @export
+hypr <- function(..., terms = NULL) {
+  hyps = list(...)
+  if(length(hyps) == 0) {
+    return(new("hypr"))
+  } else if(length(hyps) == 1 && is.list(hyps[[1]])) {
+    hyps <- hyps[[1]]
+  }
+  if(!all(vapply(hyps, function(x) is(x, "formula"), logical(1)))) {
+    stop("Arguments to hypr() must be formulas or a list() of those.")
+  }
+  parsed_hypotheses <- lapply(hyps, parse_hypothesis, valid_terms = terms)
+  hmat <- eqs2hmat(parsed_hypotheses)
+  cmat <- hmat2cmat(hmat)
+  new("hypr", eqs = parsed_hypotheses, hmat = hmat, cmat = cmat)
+}
+
+#' Retrieve or set hypothesis matrix
+#'
+#' Description
+#'
+#' @rdname hmat
+#' @param x A hypr object
+#' @param value Hypothesis matrix
+#'
+#' @export
+hmat <- function(x, ...) function(x) as.fractions(x@hmat)
+
+#' @describeIn hmat Set hypothesis matrix
+#' @export
+`hmat<-` <- function(x, value) {
+  class(value) <- setdiff(class(value), "fractions")
+  x@hmat <- value
+  x@eqs <- hmat2eqs(value)
+  x@cmat <- hmat2cmat(value)
+  x
+}
+
+
+#' Retrieve the terms (variables) used in a hypr object
+#'
+#' @param x
+#'
+#' @export
+setMethod("terms", signature(x="hypr"), function(x) rownames(hmat(x)))
+
+#' Retrieve or set contrast matrix
+#'
+#' Description
+#'
+#' @param x A hypr object
+#' @param value contrast matrix
+#' @rdname cmat
+#'
+#' @export
+cmat <- function(x, add_intercept = FALSE, remove_intercept = FALSE) {
+  if(add_intercept && remove_intercept) {
+    stop("Cannot add and remove intercept at the same time!")
+  } else if(add_intercept) {
+    as.fractions(cbind(1, x@cmat))
+  } else if(remove_intercept) {
+    as.fractions(x@cmat[,-1,drop=F])
+  } else {
+    as.fractions(x@cmat)
+  }
+}
+
+#' @describeIn cmat Set contrast matrix
+#' @export
+`cmat<-` <- function(x, value, add_intercept = FALSE, remove_intercept = FALSE) {
+  if(!is.matrix(value)) {
+    stop("`value` must be a contrast matrix!")
+  }
+  if(add_intercept && remove_intercept) {
+    stop("Cannot add and remove intercept at the same time!")
+  } else if(add_intercept) {
+    if(!is.matrix(value)) stop("add_intercept=TRUE can only be used with a matrix argument!")
+    value <- cbind(1, value)
+  } else if(remove_intercept) {
+    if(!is.matrix(value)) stop("remove_intercept=TRUE can only be used with a matrix argument!")
+    value <- value[,-1,drop=F]
+  }
+  if(is.null(rownames(value))) {
+    rownames(value) <- sprintf("mu%d", seq_len(nrow(value)))
+  } else {
+    rownames(value) <- vapply(rownames(value), function(s) if(grepl("^[^A-Za-z_]", s)) paste0("mu",s) else s, character(1))
+  }
+  class(value) <- setdiff(class(value), "fractions")
+  x@cmat <- value
+  x@hmat <- cmat2hmat(value)
+  x@eqs <- hmat2eqs(value)
+  x
+}
+
+
+#' @describeIn cmat Retrieve contrast matrix to override factor contrasts
+#' @export
+contr.hypothesis <- function(..., add_intercept = FALSE, remove_intercept = FALSE) {
+  args <- list(...)
+  if(length(args) == 1 && is.numeric(args[[1]])) {
+    contr.treatment(args[[1]])
+  } else if(length(args) == 1 && is(args[[1]], "hypr")) {
+    cmat(x = args[[1]], add_intercept = add_intercept, remove_intercept = remove_intercept)
+  } else {
+    cmat(x = hypr(...), add_intercept = add_intercept, remove_intercept = remove_intercept)
+  }
+}
